d11cc81916d46d8989b37d1ad0639f1e3dfaed84
Taarna <ivana.rast@gmail.com>
2017-10-06 10:27:36 +0200

    - Using custom decoder to decod different date formats.

--- Swift4JSONPlayground.playground/Contents.swift
+++ Swift4JSONPlayground.playground/Contents.swift
@@ -138,3 +138,87 @@ do {
 } catch let error {
     print("error happened: \(error)")
 }
+
+//: Different date formats
+jsonString = """
+{
+    "date_short": "10/03/17",
+    "date_medium": "10/03/17 10:28",
+    "date_long": "2017-10-03T10:28:07.231Z"
+}
+"""
+
+incomingData = jsonString.data(using: .utf8)!
+
+struct MrDate: Codable {
+    let dateShort: Date
+    let dateMedium: Date
+    let dateLong: Date
+
+    var description: String {
+        return "dateShort:\(dateShort), dateLong:\(dateMedium), dateLong:\(dateLong)"
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case dateShort = "date_short"
+        case dateMedium = "date_medium"
+        case dateLong = "date_long"
+    }
+}
+
+enum DateFormat: String {
+    case short = "MM/dd/yy"
+    case medium = "MM/dd/yy hh:mm"
+    case long = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+}
+extension DateFormatter {
+    convenience init(format: DateFormat) {
+        self.init()
+
+        self.dateFormat = format.rawValue
+        self.locale = Locale(identifier: "en_US_POSIX")
+        self.timeZone = TimeZone(abbreviation: "GMT")
+    }
+    static var short: DateFormatter {
+        return DateFormatter(format: .short)
+    }
+    static var medium: DateFormatter {
+        return DateFormatter(format: .medium)
+    }
+    static var long: DateFormatter {
+        return DateFormatter(format: .long)
+    }
+}
+
+let myDecoder: JSONDecoder = {
+    let decoder = JSONDecoder()
+
+    decoder.dateDecodingStrategy = .custom({ (decoder) -> Date in
+        let container = try decoder.singleValueContainer()
+        let dateToDecode = try container.decode(String.self)
+
+        var date: Date?
+
+        if let d = DateFormatter.short.date(from: dateToDecode) {
+            date = d
+        } else if let d = DateFormatter.medium.date(from: dateToDecode) {
+            date = d
+        } else if let d = DateFormatter.long.date(from: dateToDecode) {
+            date = d
+        }
+
+        guard let d = date else {
+            throw DecodingError.dataCorruptedError(in: container, debugDescription: "Cannot decode date string \(dateToDecode)")
+        }
+
+        return d
+    })
+    return decoder
+}()
+
+do {
+    let mrDate = try myDecoder.decode(MrDate.self, from: incomingData)
+    print(mrDate.description)
+} catch let error {
+    print("error happened: \(error)")
+}
